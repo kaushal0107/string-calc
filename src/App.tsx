3c652f11915990ad71099ee8edb69cdc57d216a1
Kaushal <59397619+kaushal0107@users.noreply.github.com>
2024-12-07 11:29:19 +0530

    Initiated Frontend setup using Reactjs, TailwindCSS, postcss, autoprefixer

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-center text-purple-700 mb-6">
+        String Calculator
+      </h1>
+    </>
+  );
+}
+
+export default App;
