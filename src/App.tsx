0317b6c3918b76d79e5e500c1761db6fb50b9b22
Kaushal <59397619+kaushal0107@users.noreply.github.com>
2024-12-07 11:42:28 +0530

    Added tailwindcss config and removed unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 const App = () => {
+
   return (
     <>
-      <h1 className="text-4xl font-bold text-center text-purple-700 mb-6">
-        String Calculator
-      </h1>
+    <h1 className="text-4xl font-bold text-center text-purple-700 mb-6">
+          String Calculator
+        </h1>
     </>
   );
 }
