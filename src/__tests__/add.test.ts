c12cbd9872cc60b4d9376ce5509bba31329a1665
“Kaushal <kaushal.mishra@storeapss.org>
2024-12-07 10:35:30 +0530

    support common separated, zero number

--- src/__tests__/add.test.ts
+++ src/__tests__/add.test.ts
@@ -1,5 +1,21 @@
-import { add } from '../add';
+import { add } from '../utils/add';
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(add(1, 2)).toBe(3);
+test('returns 0 for an empty string', () => {
+    expect(add("")).toBe(0);
+});
+
+test('returns the number itself if the input contains one number', () => {
+    expect(add("1")).toBe(1);
+});
+
+test('returns the sum of two numbers', () => {
+    expect(add("1,2")).toBe(3);
+});
+
+test('returns the sum of multiple numbers', () => {
+    expect(add("1,2,3,4,5")).toBe(15);
+});
+
+test('handles spaces in input string', () => {
+    expect(add("1, 2, 3")).toBe(6);
 });
